985e9abf973353b28e8c0904b656e572829e99d7
Grzegorz Piotrowski <gpiotrowski@users.noreply.github.com>
2017-09-28 19:34:44 +0200

    Basic project structure

--- CustomerAssignment.Customers.API/Program.cs
+++ CustomerAssignment.Customers.API/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace CustomerAssignment
+namespace CustomerAssignment.Customers.API
 {
     public class Program
     {
